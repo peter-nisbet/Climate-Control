38b64f64ad2bda19bb20683c40814bed6110be60
peter-nisbet <peternisbet265@live.ca>
2017-08-04 10:16:02 -0400

    Removed thermocouple code and using SI7021 sensor
    Removed the dual conditions for the temperature sensor now code only uses the SI7021 sensor.

--- ClimateControl/ClimateControl.ino
+++ ClimateControl/ClimateControl.ino
@@ -1,23 +1,18 @@
 #include <SparkFun_Si7021_Breakout_Library.h>
 #include <Wire.h>
 
-#define ThermocoupleUse 1   //Set to 1 if you want to run test without temperature sensor.
-
-//float humidity = 0;
+float humidity = 0;
 float tempC = 0;
-int speedVal = 0;
+int speedVal1 = 20;   //Speed value for exhaust fan
+int speedVal2 = 0;    //Speed value for stimulation fan
+int speedVal3 = 0;    //Speed value for input fan
+
 long entry = 0;
 
 //Change time of polling system in ms intervals.
 unsigned long timeVal = 20000;  
 
-int power = A3;
-int GND = A2;
-
-#ifdef ThermocoupleUse
-int tempSense = A4;
 char fanSpeed[7];
-#endif
 
 int fan1 = 3;   //Exhaust Fan
 int fan2 = 5;   //Stimulation Fan
@@ -28,41 +23,25 @@ Weather sensor;
 //Function prototypes
 void tempVal();
 void printText();
-#ifdef ThermocoupleUse
-  void thermocoupleTemp();
-  void fanControlLoop();
-#endif
+void fanControlLoop();
 
 void setup() {
-  // put your setup code here, to run once:
 
   Serial.begin(9600); //Serial port begin at 9600 baud
 
   //Pin mode setup//
-  pinMode(power, HIGH);             //Power
-  pinMode(GND, LOW);              //GND
 
   pinMode(fan1, OUTPUT);
   pinMode(fan2, OUTPUT);
   pinMode(fan3, OUTPUT);
 
-  digitalWrite(power, HIGH);
-  digitalWrite(GND, LOW);
-
   //Begin Silabs sensor
-  #ifndef ThermocoupleUse
-    sensor.begin();
-  #endif
-
+  sensor.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  #ifndef ThermocoupleUse
-   tempVal();      //Get current temp value
-  #else
-    thermocoupleTemp();
-  #endif
+  
+  tempVal();      //Get current temp value
 
   fanControlLoop();
   
@@ -70,88 +49,92 @@ void loop() {
   printText();
 
   delay(timeVal); //Sampling delay
- /*if(tempC>=25 && tempC<30){
-    fanSpeedCon(256); //25% duty cycle
- }
- else if(tempC>=30 && tempC<40){
-    fanSpeedCon(768); //75% duty cycle
- }
- else if(tempC<25){
-    fanSpeedCon(0); //0% duty cycle
- }
- else if(tempC>=40){
-    fanSpeedCon(1023);  //100% duty cycle
- }*/
 }
 
 void tempVal(){
- // humidity = sensor.getRH();  //Triggers reading temperature and humidity
+  humidity = sensor.getRH();  //Triggers reading temperature and humidity
   tempC = sensor.getTemp();   //Reading temperature value from last humidity reading
 }
 
 int fanSpeedCon(int a){
-  analogWrite(fan1, a);
-  analogWrite(fan2, a);
-  analogWrite(fan3, a);
+
+  if(a == 0){
+     speedVal1 = a+23; //As the exhaust fan can't be off.
+     speedVal2 = a+73; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+     
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }
+  else if(a == 64){
+     speedVal1 = a+35; //As the exhaust fan can't be off.
+     speedVal2 = a-12; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+         
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }
+  else if(a == 192){
+     speedVal1 = a+40; //As the exhaust fan can't be off.
+     speedVal2 = a-152; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+         
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }
+  else if(a == 255){
+     speedVal1 = a; //As the exhaust fan can't be off.
+     speedVal2 = 0; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a-25;   //Input fan is off.
+         
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }  
 }
 
 void printText(){
   Serial.print("Entry:");
-  Serial.print(entry);
-  Serial.print("\n");
+  Serial.println(entry);
   Serial.print("Temp:");
   Serial.print(tempC);
-  #ifndef ThermocoupleUse
-    Serial.print("C\n");
-  #else
-    Serial.print("\n");
-  #endif
-    Serial.print("Current Fan Speed:");
-    Serial.print(fanSpeed);  
-    Serial.print("\n\n");
-}
-
-void thermocoupleTemp(){
-  tempC = analogRead(tempSense);  //Raw ADC value, Need to convert to actual temp
+  Serial.println("C");
+  Serial.print("Humidity:");
+  Serial.print(humidity);
+  Serial.println("%");
+  Serial.print("Current Fan Speed:");
+  Serial.println(fanSpeed); 
+  Serial.print("Fan 1 speed:");
+  Serial.println(speedVal1); 
+  Serial.print("Fan 2 speed:");
+  Serial.println(speedVal2);
+  Serial.print("Fan 3 speed:");
+  Serial.print(speedVal3);        
+  Serial.print("\n\n");
 }
 
 void fanControlLoop(){
-  #ifdef  ThermocoupleUse
+
     memset(fanSpeed, 0, sizeof(fanSpeed));
     
-     if(tempC>=220 && tempC<420){
-       //fanSpeedCon(256); //25% duty cycle
-       fanSpeedCon(64); //25% duty cycle
-       strcpy(fanSpeed, "Low");
-     }
-     else if(tempC>=420 && tempC<600){
-      //fanSpeedCon(768); //75% duty cycle
-      fanSpeedCon(192); //75% duty cycle
+    if(tempC>=18 && tempC<22){
+      fanSpeedCon(64); //25% duty cycle
+      strcpy(fanSpeed, "Low");
+    }
+    else if(tempC>=22 && tempC<30){
+       fanSpeedCon(192); //75% duty cycle
        strcpy(fanSpeed, "Medium");
-     }
-     else if(tempC<220){
-       //fanSpeedCon(0); //0% duty cycle
+    }
+    else if(tempC<18){
        fanSpeedCon(0); //0% duty cycle
        strcpy(fanSpeed, "Off");
-     }
-     else if(tempC>=600){
-       //fanSpeedCon(1023);  //100% duty cycle
+    }
+    else if(tempC>=30){
        fanSpeedCon(255);  //100% duty cycle
        strcpy(fanSpeed, "High");
-     }
-  #else
-    if(tempC>=25 && tempC<30){
-      fanSpeedCon(256); //25% duty cycle
-    }
-    else if(tempC>=30 && tempC<40){
-       fanSpeedCon(768); //75% duty cycle
-    }
-    else if(tempC<25){
-       fanSpeedCon(0); //0% duty cycle
-    }
-    else if(tempC>=40){
-       fanSpeedCon(1023);  //100% duty cycle
-    } 
-  #endif 
+    }  
 }
 
