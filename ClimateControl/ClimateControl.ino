04b194efb24f2eaa177e32b29fb6b900ad1ed9fb
peter-nisbet <peternisbet265@live.ca>
2017-08-21 09:15:00 -0400

    Changed PWM values.
    Changed the PWM values for the motor control to make all three channels track. Also added more pwm values to for controlling the fan speed.

--- ClimateControl/ClimateControl.ino
+++ ClimateControl/ClimateControl.ino
@@ -59,6 +59,64 @@ void tempVal(){
 int fanSpeedCon(int a){
 
   if(a == 0){
+     speedVal1 = a; //As the exhaust fan can't be off.
+     speedVal2 = a; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+     
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }
+  else if(a == 70){
+     speedVal1 = a; //As the exhaust fan can't be off.
+     speedVal2 = a; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+         
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }
+  else if(a == 100){
+     speedVal1 = a; //As the exhaust fan can't be off.
+     speedVal2 = a; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+         
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }
+  else if(a == 130){
+     speedVal1 = a; //As the exhaust fan can't be off.
+     speedVal2 = 0; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+         
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }      
+  else if(a == 170){
+     speedVal1 = a; //As the exhaust fan can't be off.
+     speedVal2 = 0; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+         
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }    
+  else if(a == 255){
+     speedVal1 = a; //As the exhaust fan can't be off.
+     speedVal2 = 0; //Keeps stimulation fan running a speed because the input fan is off.
+     speedVal3 = a;   //Input fan is off.
+         
+     analogWrite(fan1, speedVal1);
+     analogWrite(fan2, speedVal2);
+     analogWrite(fan3, speedVal3);    
+  }  
+}
+
+/*int fanSpeedCon(int a){
+
+  if(a == 0){
      speedVal1 = a+23; //As the exhaust fan can't be off.
      speedVal2 = a+73; //Keeps stimulation fan running a speed because the input fan is off.
      speedVal3 = a;   //Input fan is off.
@@ -94,7 +152,7 @@ int fanSpeedCon(int a){
      analogWrite(fan2, speedVal2);
      analogWrite(fan3, speedVal3);    
   }  
-}
+}*/
 
 void printText(){
   Serial.print("Entry:");
@@ -120,7 +178,7 @@ void fanControlLoop(){
 
     memset(fanSpeed, 0, sizeof(fanSpeed));
     
-    if(tempC>=18 && tempC<22){
+    /*if(tempC>=18 && tempC<22){
       fanSpeedCon(64); //25% duty cycle
       strcpy(fanSpeed, "Low");
     }
@@ -135,6 +193,31 @@ void fanControlLoop(){
     else if(tempC>=30){
        fanSpeedCon(255);  //100% duty cycle
        strcpy(fanSpeed, "High");
-    }  
+    } */
+
+    if(tempC>=21 && tempC<22){
+      fanSpeedCon(70); //25% duty cycle
+      strcpy(fanSpeed, "Low");
+    }
+    else if(tempC>=22 && tempC<23){
+       fanSpeedCon(100); //75% duty cycle
+       strcpy(fanSpeed, "Low-Medium");
+    }   
+    else if(tempC>=23 && tempC<24){
+       fanSpeedCon(130); //75% duty cycle
+       strcpy(fanSpeed, "Medium");
+    }
+    else if(tempC>=24 && tempC<25){
+       fanSpeedCon(170); //75% duty cycle
+       strcpy(fanSpeed, "Medium-High");
+    }
+    else if(tempC<21){
+       fanSpeedCon(0); //0% duty cycle
+       strcpy(fanSpeed, "Off");
+    }
+    else if(tempC>=25){
+       fanSpeedCon(255);  //100% duty cycle
+       strcpy(fanSpeed, "Max!");
+    }       
 }
 
